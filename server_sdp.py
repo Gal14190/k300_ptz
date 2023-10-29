8deaa3446a10499b06800837265c90b90f38350e
israel mark <israelmark98@gmail.com>
2023-10-29 17:02:37 +0200

    making the server more generic

--- server_sdp.py
+++ server_sdp.py
@@ -4,19 +4,16 @@ import json
 import time
 
 
-class ApiLocations:
+class ApiDataHandler:
     def __init__(self, port):
         self.port = port
-        self.yolo_data = None
-        self.sdr_data = None
+        self.data = {}
+        self.data_lock = threading.Lock()
         self.server_thread = threading.Thread(target=self.start_server)
         self.server_thread.daemon = True
+        self.running = True
 
         self.server_thread.start()
-        self.yolo_data_condition = threading.Condition()
-        self.sdr_data_condition = threading.Condition()
-        self.yolo_data_lock = threading.Lock()
-        self.sdr_data_lock = threading.Lock()
 
     def handle_client(self, client_socket, client_address):
         print(f"Accepted connection from {client_address}")
@@ -26,21 +23,16 @@ class ApiLocations:
                 break
             try:
                 received_data = json.loads(data.decode('utf-8'))
-                if received_data["source"] == "YOLO":
-                    with self.yolo_data_lock:
-                        self.yolo_data = received_data
-                        print(self.yolo_data)
-                elif received_data["source"] == "SDR":
-                    with self.sdr_data_lock:
-                        self.sdr_data = received_data
-                        print(self.sdr_data)
+                source = received_data.get("source", "Unknown")
+                with self.data_lock:
+                    self.data[source] = received_data
+                    # print(f"Received data from {source}: {self.data[source]}")
             except json.JSONDecodeError as e:
                 print(f"Error decoding JSON: {e}")
         client_socket.close()
 
     def start_server(self):
         PORT = self.port
-        self.running = True
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind(('0.0.0.0', PORT))
         server_socket.listen(1)
@@ -58,37 +50,29 @@ class ApiLocations:
         self.running = False
         print("Server has stopped")
 
-    def getSDR_data(self):
-        with self.sdr_data_lock:
-            return self.sdr_data
-
-    def getYOLO_data(self):
-        with self.yolo_data_lock:
-            return self.yolo_data
+    def get_data(self, source):
+        with self.data_lock:
+            return self.data.get(source, "No Data For Current Source Name")
 
     def wait_for_response(self, source, timeout=None):
         start_time = time.time()
         data = None
-        condition = self.yolo_data_condition if source == "YOLO" else self.sdr_data_condition
 
-        with condition:
-            while not data and (timeout is None or (time.time() - start_time) < timeout):
-                condition.wait(timeout)
-                if source == "YOLO":
-                    data = self.getYOLO_data()
-                elif source == "SDR":
-                    data = self.getSDR_data()
-            return data
+        while not data and (timeout is None or (time.time() - start_time) < timeout):
+            data = self.get_data(source)
+            if not data:
+                time.sleep(0.1)
+        return data
 
 
 # def main():
-
-#     api = ApiLocations(5678)
+#     api = ApiDataHandler(5678)
 #     try:
-
 #         while True:
-#             yolo_data = api.wait_for_response("YOLO", timeout=2)
-#             sdr_data = api.wait_for_response("SDR", timeout=2)
+#             panter = api.get_data("PANTER")
+#             time.sleep(0.3)
+#             if panter:
+#                 print(panter)
 
 #         input("Press Enter to stop the server...")
 #     except KeyboardInterrupt:
