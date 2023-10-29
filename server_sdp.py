99c621ec6f03b3dba8057a976789f781e1cbc20f
israel mark <israelmark98@gmail.com>
2023-10-29 14:53:33 +0200

    update the server side

--- server_sdp.py
+++ server_sdp.py
@@ -1,47 +1,102 @@
 import socket
 import threading
 import json
+import time
+
 
 class ApiLocations:
     def __init__(self, port):
         self.port = port
-        self.sending_data = None
+        self.yolo_data = None
+        self.sdr_data = None
         self.server_thread = threading.Thread(target=self.start_server)
         self.server_thread.daemon = True
+
         self.server_thread.start()
+        self.yolo_data_condition = threading.Condition()
+        self.sdr_data_condition = threading.Condition()
+        self.yolo_data_lock = threading.Lock()
+        self.sdr_data_lock = threading.Lock()
+
+    def handle_client(self, client_socket, client_address):
+        print(f"Accepted connection from {client_address}")
+        while self.running:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            try:
+                received_data = json.loads(data.decode('utf-8'))
+                if received_data["source"] == "YOLO":
+                    with self.yolo_data_lock:
+                        self.yolo_data = received_data
+                        print(self.yolo_data)
+                elif received_data["source"] == "SDR":
+                    with self.sdr_data_lock:
+                        self.sdr_data = received_data
+                        print(self.sdr_data)
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON: {e}")
+        client_socket.close()
 
     def start_server(self):
         PORT = self.port
+        self.running = True
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind(('0.0.0.0', PORT))
         server_socket.listen(1)
         print(f"Server is listening on port {PORT}")
 
-        while True:
+        while self.running:
             client_socket, client_address = server_socket.accept()
-            print(f"Accepted connection from {client_address}")
-            data = client_socket.recv(1024)
-            try:
-                received_data = json.loads(data.decode('utf-8'))  
-                keys_to_extract = ["latitude", "longitude", "altitude"]
-                self.sending_data = {key: received_data.get(key, None) for key in keys_to_extract}
-                # print(f"Received: {self.sending_data}")
-            except json.JSONDecodeError as e:
-                print(f"Error decoding JSON: {e}")
+            client_thread = threading.Thread(
+                target=self.handle_client, args=(client_socket, client_address))
+            client_thread.start()
 
-            client_socket.sendall(b'Message received by the server')
-            client_socket.close()
+        server_socket.close()
 
-    def location_details(self):
-        return self.sending_data
+    def stop_server(self):
+        self.running = False
+        print("Server has stopped")
+
+    def getSDR_data(self):
+        with self.sdr_data_lock:
+            return self.sdr_data
+
+    def getYOLO_data(self):
+        with self.yolo_data_lock:
+            return self.yolo_data
+
+    def wait_for_response(self, source, timeout=None):
+        start_time = time.time()
+        data = None
+        condition = self.yolo_data_condition if source == "YOLO" else self.sdr_data_condition
+
+        with condition:
+            while not data and (timeout is None or (time.time() - start_time) < timeout):
+                condition.wait(timeout)
+                if source == "YOLO":
+                    data = self.getYOLO_data()
+                elif source == "SDR":
+                    data = self.getSDR_data()
+            return data
+
 
 # def main():
-#     api = ApiLocations()
-#     api.location_details()
-#     print("check Thread")
-#     while True:
+
+#     api = ApiLocations(5678)
+#     try:
+
+#         while True:
+#             yolo_data = api.wait_for_response("YOLO", timeout=2)
+#             sdr_data = api.wait_for_response("SDR", timeout=2)
+
+#         input("Press Enter to stop the server...")
+#     except KeyboardInterrupt:
 #         pass
+#     finally:
+#         api.stop_server()
+#         api.server_thread.join()
+
 
 # if __name__ == "__main__":
 #     main()
-
